52504e2eb616f66a6ce565560d151a6b0ee9e38d
“Caleb <calebh27@byu.edu>
2021-08-28 11:35:42 -0600

    shift results to scratch category

--- crontab_script.py
+++ crontab_script.py
@@ -1,13 +1,13 @@
 """
-Use this as the command you use to launch the qlaunch singleshot (see the crontab_setup.md file). 
-It confirms that you have not exceeded the number of jobs that you would like to have running in 
-the que before launching another rocket.
-NOTE: 
+Use this as the command you use to launch the qlaunch singleshot (see the crontab_setup.md file). It confirms that
+you have not exceeded the number of jobs that you would like to have running in the que
+before launching another rocket.
+NOTE:
 - You will need to change the path to your username
 - Put this script in your home directory
 """
 
-MAX_IN_QUE = 20
+MAX_IN_QUE = 24
 
 import os
 import subprocess
@@ -17,6 +17,9 @@ os.chdir("/zhome/calebh27")
 
 # activate the atomate_env
 os.system("source /zhome/calebh27/atomate/atomate_env/bin/activate")
+
+# change the directory to be atomate/scratch
+os.chdir("/fslhome/calebh27/atomate/scratch")
 
 # get the number of lines that are returned by the squeue command, which effectively
 # tells you how many things are currently running.
@@ -29,3 +32,4 @@ if jobs_running <= MAX_IN_QUE:
     print(launch_info)
 else:
     print("There are already", MAX_IN_QUE, "jobs running")
+
